f98e41f714399a7d630537bd84e0727ed7105387
edwardkerry <edwardkerry@gmail.com>
2016-02-22 12:08:29 +0000

    Allows items to be added to orders

--- spec/OrderSpec.js
+++ spec/OrderSpec.js
@@ -3,11 +3,17 @@ describe('Order', function(){
   var order;
 
   beforeEach(function(){
-    order = new Order;
+    order = new Order();
   });
 
   it('should initialize with an empty order', function(){
     expect(order.items).toEqual([]);
   });
+
+  it('should allow items to be added to the order', function(){
+    order.add('Tea', 1);
+    expect(order.items[0]).toEqual({'Tea': 1});
+  });
+
 
 });
--- src/order.js
+++ src/order.js
@@ -1,3 +1,9 @@
 function Order(){
   this.items = [];
 }
+
+Order.prototype.add = function(item, qty){
+  var choice = {};
+  choice[item] = qty;
+  this.items.push(choice);
+};
